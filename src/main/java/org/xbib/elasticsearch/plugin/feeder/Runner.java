543c36b3e6aa877ab4bb04b901415cf2d1361c82
proofy <sj.at.soit@gmail.com>
2014-10-15 09:25:10 +0200

    2nd start argument to give a JDBC Class loading on startup

--- src/main/java/org/xbib/elasticsearch/plugin/feeder/Runner.java
+++ src/main/java/org/xbib/elasticsearch/plugin/feeder/Runner.java
@@ -3,21 +3,33 @@ package org.xbib.elasticsearch.plugin.feeder;
 import java.io.InputStreamReader;
 
 /**
- * Stub for loading a class (tool) and execute it. Isolating the main method from other methods is of advantage
- * for JVMs that thoroughly check if this class ca be executed.
+ * Stub for loading a class (tool) and execute it. Isolating the main method from other methods is of advantage for JVMs that thoroughly check if this class ca
+ * be executed.
  */
 public class Runner {
 
-    public static void main(String[] args) {
-        try {
-            Class clazz = Class.forName(args[0]);
-            CommandLineInterpreter commandLineInterpreter = (CommandLineInterpreter) clazz.newInstance();
-            Runtime.getRuntime().addShutdownHook(commandLineInterpreter.shutdownHook());
-            commandLineInterpreter.readFrom(new InputStreamReader(System.in, "UTF-8")).run();
-        } catch (Throwable e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
-        System.exit(0);
-    }
+	public static void main(String[] args) {
+		try {
+			Class clazz = Class.forName(args[0]);
+			if (args[1] != null) {
+				try {
+					// load given JDBC driver
+					Class.forName(args[1]);
+				}
+				catch (Exception e) {
+					System.err.println(args[1]);
+					e.printStackTrace();
+					System.exit(2);
+				}
+			}
+			CommandLineInterpreter commandLineInterpreter = (CommandLineInterpreter) clazz.newInstance();
+			Runtime.getRuntime().addShutdownHook(commandLineInterpreter.shutdownHook());
+			commandLineInterpreter.readFrom(new InputStreamReader(System.in, "UTF-8")).run();
+		}
+		catch (Throwable e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		System.exit(0);
+	}
 }
